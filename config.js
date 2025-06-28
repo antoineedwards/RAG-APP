9051803304164f033c1b032743254f0b8ca7d084
antoineedwards <150852309+antoineedwards@users.noreply.github.com>
2025-06-28 08:17:05 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+/** @next/config */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/:path*',
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
